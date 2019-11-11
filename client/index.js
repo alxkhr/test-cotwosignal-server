3682186ba37e0b1d7aea51cec3932bbf868c3c65
Alexander Kehr <alex@alexanderkehr.de>
2019-11-11 23:25:19 +0100

    sort signals

--- client/index.js
+++ client/index.js
@@ -1,6 +1,12 @@
 async function render() {
   const response = await fetch('/signals');
-  console.log('signals', response);
+  if (response.ok) {
+    const signals = await response.json();
+    console.log(
+      'signals',
+      signals.sort((a, b) => (a.timestamp > b.timestamp ? 1 : b.timestamp > a.timestamp ? -1 : 0)),
+    );
+  }
 }
 
 render();
