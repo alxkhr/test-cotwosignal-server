c44fabb340c3396cacbbc6e604706b219737cbe5
Alexander Kehr <alex@alexanderkehr.de>
2019-11-11 17:19:12 +0100

    connect to cotwosignal

--- server.js
+++ server.js
@@ -6,6 +6,30 @@ const db = {
   signals: new Datastore({ filename: '.data/signals', autoload: true }),
 };
 
+// TODO remove if working
+db.signals.remove({}, { multi: true }, (err, numRemoved) => {});
+loadFirstSignal();
+
+async function loadFirstSignal() {
+  const response = await fetch('', { headers: { 'auth-token': process.env.COTWO_API_KEY } });
+  if (response.ok) {
+    const resContent = await response.json();
+    await insertSignal({
+      timestamp: new Date().toISOString(),
+      status: resContent.status,
+      intensity: resContent.data.carbonIntensity,
+      good: 100 - resContent.data.fossilFuelPercentage,
+    });
+  } else {
+    await insertSignal({
+      timestamp: new Date().toISOString(),
+      status: 'server not responding',
+      intensity: -1,
+      good: -1,
+    });
+  }
+}
+
 async function insertSignal(doc) {
   return new Promise((resolve, reject) => {
     db.signals.insert(doc, (err, newDoc) => (err ? reject(err) : resolve(newDoc)));
